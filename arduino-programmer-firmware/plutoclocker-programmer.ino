85a8404cfc2247e201fdf918cd478af952531927
critbit <js@vcc.earth>
2020-12-19 17:29:50 +0100

    setting proper clock outputs for the chip

--- arduino-programmer-firmware/plutoclocker-programmer.ino
+++ arduino-programmer-firmware/plutoclocker-programmer.ino
@@ -1,4 +1,29 @@
+/*  CDCE913 programmer code for Arduino
+  ========================================================
+  Intended for programing CDCE913 as 25 MHz clock reference for LNB and 40 MHz for ADALM Pluto
+  
+  Ver. 0.5
 
+Copyright (c) 2020 Mirosław Sadowski, Jakub Sadowski
+
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software 
+is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included 
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+  
 #include <Wire.h>
 
 void setup() {
@@ -6,20 +31,18 @@ void setup() {
   
   Wire.begin();
 
-  //may be used for debug purposes
-  //setup_4MHZ_clk();
+  // 4 MHz generator may be used for debug purposes - uncomment the line below
+  // setup_4MHZ_clk();
   
   program_CDCE913();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(750);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(750);
+  digitalWrite(LED_BUILTIN, LOW);
   delay(250);
 }
-
-
 
 
 void setup_4MHZ_clk(){
@@ -35,7 +58,7 @@ void program_CDCE913(){
   /*
    * Based on http://sp9uob.verox.pl/pliki/fiberref.pdf
    * 
-   * These example values enable 10,25 and 100MHz outputs, when **10MHz** input is connected.
+   * These example values enable Y1=10, Y2=25 and Y3=40 MHz outputs, when 10 MHz CLK nput is connected.
    */
   
   uint8_t addr = 0x65;
@@ -43,7 +66,7 @@ void program_CDCE913(){
     0x01, 0x06, 0x05, 0x34, 0x01, 0x02, 0x50
   };
   uint8_t comm2[14] = {
-    0x14, 0x0c, 0x6D, 0x02, 0x08, 0x02, 0xff,
+    0x14, 0x0c, 0x6D, 0x02, 0x08, 0x05, 0xff,
     0x00, 0x02, 0x83, 0xff, 0x00, 0x02, 0x80
   };
   uint8_t comm_save_eeprom[2] = {0x86, 0x01};
